a1fae833df04c0ec7fee1bdbc5ec13cbcf2fda9a
Christian <kalonjic34@gmail.com>
2025-05-09 17:26:19 +0200

    Add redirect logic for invalid month in monthly challenge view

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 
 monthly_challenges = {
     "january": "Eat no meat for the entire month!",
@@ -19,7 +20,14 @@ monthly_challenges = {
 # Create your views here.
 
 def monthly_challenge_by_number(request,month):
-    return HttpResponse(month)
+    months = list(monthly_challenges.keys())
+    
+    if month > len(months):
+        return HttpResponseNotFound("invalid month")
+    
+    redirect_month = months[month -1]
+    redirect_path = reverse("month-challenge",args=[redirect_month]) #/challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 def monthly_challenge(request,month):
     try:
