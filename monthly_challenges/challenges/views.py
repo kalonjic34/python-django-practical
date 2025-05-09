5d6cae45f4c88af5247509cdfa59bcf285fe2d83
Christian <kalonjic34@gmail.com>
2025-05-09 16:25:24 +0200

    Refactor monthly challenge views to consolidate logic into a single function

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,10 +1,16 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
 
-def jan(request):
-    return HttpResponse("Eat no meat for the entire month!")
-
-def feb(request):
-    return HttpResponse("walk for at least 20 minutes every day!")
+def monthly_challenge(request,month):
+    challenge_text = None
+    if month == "jan":
+        challenge_text = "Eat no meat for the entire month!"
+    elif month == "feb":
+        challenge_text = "walk for at least 20 minutes every day!"
+    elif month == "march":
+        challenge_text = "Learn Django for atleast 20 minutes every day!"
+    else:
+        return HttpResponseNotFound("This month is not supported!")
+    return HttpResponse(challenge_text)
