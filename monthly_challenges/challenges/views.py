af006bc60185960899092c9743b69429395a1b53
Christian <kalonjic34@gmail.com>
2025-05-12 12:32:29 +0200

    Refactor URL patterns to include name for index view and improve error handling in monthly challenge view

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+# from django.template.loader import render_to_string
 
 
 monthly_challenges = {
@@ -44,5 +45,5 @@ def monthly_challenge(request,month):
             "month_name": month
         })
     except:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        raise Http404()
     
