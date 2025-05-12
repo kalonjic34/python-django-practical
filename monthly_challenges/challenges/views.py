b8610db48b1a29c5589c0b383ea3c4421f609c11
Christian <kalonjic34@gmail.com>
2025-05-12 11:59:54 +0200

    Refactor index view to use render function and update December challenge description

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -15,23 +15,16 @@ monthly_challenges = {
     "september": "Learn a new language for at least 15 minutes every day!",
     "october": "Participate in a daily gratitude challenge!",
     "november": "Limit screen time to 1 hour a day!",
-    "december": "Reflect on your year and set goals for next year!"
+    "december": None
 }
 
 # Create your views here.
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
-    
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-    
-    
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html",{
+        "months": months
+    })
 
 def monthly_challenge_by_number(request,month):
     months = list(monthly_challenges.keys())
